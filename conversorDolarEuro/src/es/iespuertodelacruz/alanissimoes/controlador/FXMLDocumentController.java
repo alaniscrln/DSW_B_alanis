50d76ebcdf98374638505b77d36544019adb1449
Alanis <alanis.crln@gmail.com>
2020-10-01 19:30:12 +0100

    Proyecto hecho con botones. funcional

--- conversorDolarEuro/src/es/iespuertodelacruz/alanissimoes/controlador/FXMLDocumentController.java
+++ conversorDolarEuro/src/es/iespuertodelacruz/alanissimoes/controlador/FXMLDocumentController.java
@@ -7,13 +7,10 @@ package es.iespuertodelacruz.alanissimoes.controlador;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
-import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextField;
 
 /**
@@ -21,54 +18,42 @@ import javafx.scene.control.TextField;
  * @author Alanis
  */
 public class FXMLDocumentController implements Initializable {
-    
+
     @FXML
     private TextField cantidad;
     @FXML
-    private ChoiceBox<String> moneda;
+    private Button aEuro;
     @FXML
-    private Button igual; 
+    private Button aDolar;
     @FXML
     private TextField resultado;
-    
+
     @FXML
-    private void calcular(ActionEvent event) {
-        
-        String op = this.moneda.getValue();
-        double cambio;
-        
-        switch (op) {
-            case "$":        
-                cambio = getCantidad() * 0.85;
-                this.resultado.setText( cambio + " €" );
-                break;
-            case "€":
-                cambio = getCantidad() * 1.17;
-                this.resultado.setText( cambio + " $" );
-                break;
-            default:
-                throw new AssertionError();
-        }
-        
+    private void dolarAEuro(ActionEvent e) {
+        double cambio = getCantidad() * 0.85;
+        this.resultado.setText(cambio + " €");
     }
     
-    private double getCantidad(){
+    @FXML
+    private void euroADolar(ActionEvent e) {
+        double cambio = getCantidad() * 1.17;
+        this.resultado.setText(cambio + " $");
+    }
+
+    private double getCantidad() {
         double dinero;
-        try{
+        try {
             dinero = Double.parseDouble(this.cantidad.getText());
-        }catch (Exception ex) {
+        } catch (Exception ex) {
             dinero = Double.NaN;
         }
         return dinero;
     }
-    
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-        ObservableList<String> options = FXCollections.observableArrayList("$","€");
-        moneda.setItems(options);
-        moneda.setValue("$");
 
-    }    
-    
+    }
+
 }
