d571c15b73ea6c01baad1d704c26904a55f1e774
Alanis <alanis.crln@gmail.com>
2020-10-01 19:00:02 +0100

    Vista creada

--- conversorDolarEuro/src/es/iespuertodelacruz/alanissimoes/controlador/FXMLDocumentController.java
+++ conversorDolarEuro/src/es/iespuertodelacruz/alanissimoes/controlador/FXMLDocumentController.java
@@ -7,10 +7,14 @@ package es.iespuertodelacruz.alanissimoes.controlador;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
 
 /**
  *
@@ -19,17 +23,26 @@ import javafx.scene.control.Label;
 public class FXMLDocumentController implements Initializable {
     
     @FXML
-    private Label label;
+    private TextField cantidad;
+    @FXML
+    private ChoiceBox moneda;
+    @FXML
+    private Button igual; 
+    @FXML
+    private TextField resultado;
     
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+    private void calcular(ActionEvent event) {
+        
     }
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+        ObservableList<String> options = FXCollections.observableArrayList("$","€");
+        moneda.setItems(options);
+        moneda.setValue("$");
+
     }    
     
 }
